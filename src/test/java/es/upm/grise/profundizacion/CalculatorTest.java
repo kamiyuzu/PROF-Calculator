b25255ab14ff6129ed6256813e97f64a46d304d1
odieste <odieste@fi.upm.es>
2023-09-27 17:05:38 +0200

    Ejercicio preparado

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -10,20 +10,7 @@ class CalculatorTest {
 
 	@Test
 	void test() {
-		Calculator c = new Calculator();
-		c.numbers = new Vector <Double>();
-		c.add(2.0);
-		c.add(4.0);
-		c.add(3.0);
-		System.out.println(c.max());
-		System.out.println(c.min());
-		System.out.println(c.average());
-		System.out.println(c.stddev());
-		c.remove(3.0);
-		System.out.println(c.max());
-		System.out.println(c.min());
-		System.out.println(c.average());
-		System.out.println(c.stddev());
+		fail();
 	}
 
 }
