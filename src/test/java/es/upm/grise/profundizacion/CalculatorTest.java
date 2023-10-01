3c0569c3d7b4cc7d02ee12a23ba6ccb7af80e764
kamiyuzu <alberto.rev.a@gmail.com>
2023-10-01 23:26:26 +0200

    Testing WIP 12345DN

--- src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
+++ src/test/java/es/upm/grise/profundizacion/CalculatorTest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.Vector;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -21,6 +22,7 @@ class CalculatorTest {
 	}
 
 	@Test()
+	@DisplayName("Test init the calculator state")
 	void initCalculator() {
 		Double numberToAddAndRemove = 2.0;
 		Vector<Double> expected_state = new Vector<Double>();
@@ -30,6 +32,7 @@ class CalculatorTest {
 	}
 
 	@Test()
+	@DisplayName("Test add 2.0 in the calculator state")
 	void addWithNumbers() {
 		Double numberToAddAndRemove = 2.0;
 		Vector<Double> expected_state = new Vector<Double>();
@@ -37,76 +40,85 @@ class CalculatorTest {
 		expected_state.add(numberToAddAndRemove);
 		expected_state.add(numberToAddAndRemove);
 		calculator.add(numberToAddAndRemove);
-		assertEquals(expected_state, calculator.numbers, "Expected " + state + " but got: " + calculator.numbers);
+		assertEquals(expected_state, calculator.numbers);
 	}
 
 	@Test()
+	@DisplayName("Test remove 2.0 in the calculator state")
 	void removeWithNumbers() {
 		Double numberRemove = 2.0;
 		Vector<Double> expected_state = new Vector<Double>();
 		expected_state.add(numberRemove);
 		calculator.remove(numberRemove);
-		assertEquals(expected_state, calculator.numbers, "Expected " + state + " but got: " + calculator.numbers);
+		assertEquals(expected_state, calculator.numbers);
 	}
 
 	@Test()
+	@DisplayName("Test remove without numbers in the calculator state")
 	void removeWithNoNumbers() {
 		assertThrows(ArrayIndexOutOfBoundsException.class, () -> {
-			calculator.remove(1.0);
-		}, "Expected throw when no numbers in the calculator");
+			calculator.remove(-1.0);
+		});
 	}
 
 	@Test()
+	@DisplayName("Test max without state in the calculator")
 	void maxWithNoNumbers() {
 		Double expectedMax = Double.MIN_VALUE;
 		setNewCalculator();
-		assertEquals(expectedMax, calculator.max(), "Expected max:" + expectedMax + " but got: " + calculator.max());
+		assertEquals(expectedMax, calculator.max());
 	}
 
 	@Test()
+	@DisplayName("Test max with 2.0 and 2.0 in the calculator state")
 	void maxWithNumbers() {
 		Double expectedMax = 2.0;
-		state.add(expectedMax);
-		assertEquals(expectedMax, calculator.max(), "Expected max:" + expectedMax + "but got: " + calculator.max());
+		assertEquals(expectedMax, calculator.max());
 	}
 
 	@Test()
+	@DisplayName("Test min without state in the calculator")
 	void minWithNoNumbers() {
 		Double expectedMin = Double.MAX_VALUE;
 		setNewCalculator();
-		assertEquals(expectedMin, calculator.min(), "Expected min:" + expectedMin + "but got: " + calculator.min());
+		assertEquals(expectedMin, calculator.min());
 	}
 
 	@Test()
+	@DisplayName("Test min with 2.0 and 2.0 in the calculator state")
 	void minWithNumbers() {
 		Double expectedMin = 2.0;
-		assertEquals(expectedMin, calculator.min(), "Expected min:" + expectedMin + "but got: " + calculator.min());
+		assertEquals(expectedMin, calculator.min());
 	}
 
 	@Test()
+	@DisplayName("Test average without state in the calculator")
 	void averageWithNoNumbers() {
 		Double expectedAverage = Double.NaN;
 		setNewCalculator();
-		assertEquals(expectedAverage, calculator.average(), "Expected average:" + expectedAverage + "but got: " + calculator.average());
+		assertEquals(expectedAverage, calculator.average());
 	}
 
 	@Test()
+	@DisplayName("Test average with 2.0 and 2.0 in the calculator state")
 	void averageWithNumbers() {
 		Double expectedAverage = 2.0;
-		assertEquals(expectedAverage, calculator.average(), "Expected average:" + expectedAverage + "but got: " + calculator.average());
+		assertEquals(expectedAverage, calculator.average());
 	}
 
 	@Test()
+	@DisplayName("Test stddev without state in the calculator")
 	void stddevWithNoNumbers() {
 		Double expectedStddev = -0.0;
 		setNewCalculator();
-		assertEquals(expectedStddev, calculator.stddev(), "Expected stddev:" + expectedStddev + "but got: " + calculator.stddev());
+		assertEquals(expectedStddev, calculator.stddev());
 	}
 
 	@Test()
+	@DisplayName("Test stddev with 2.0 and 2.0 in the calculator state")
 	void stddevWithNumbers() {
 		Double expectedStddev = 0.0;
-		assertEquals(expectedStddev, calculator.stddev(), "Expected stddev:" + expectedStddev + "but got: " + calculator.stddev());
+		assertEquals(expectedStddev, calculator.stddev());
 	}
 
 	private void setNewCalculator(){
